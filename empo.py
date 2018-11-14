f62d966868f4fd93daa0533ba7a977e7268d9d8a
Your Name <you@example.com>
2018-11-14 15:10:15 +0530

    newgit

--- empo.py
+++ empo.py
@@ -9,7 +9,7 @@ def index():
     return render_template('home.html')
 @app.route('/employee')
 def employee():
-    return render_template('empdata.html',myemployee=getemployee)  
+    return render_template('employee.html',myemployee=getemployee)  
 
 if(__name__=='__main__'):
     app.run(debug=True)      
